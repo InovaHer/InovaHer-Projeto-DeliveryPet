e0ba7e2bcd6a6ce92305a12dcf03f8f706461ce8
Patricia Eliseu <patyeliseu4@gmail.com>
2025-10-22 15:56:53 -0300

    add.

--- src/main/java/com/generation/deliverypet/model/Produto.java
+++
@@ -1,5 +0,0 @@
-package com.generation.deliverypet.model;
-
-public class Produto {
-
-}
--- src/main/java/com/generation/deliverypet/model/Usuario.java
+++ src/main/java/com/generation/deliverypet/model/Usuario.java
@@ -1,16 +1,14 @@
 package com.generation.deliverypet.model;
 
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -39,9 +37,9 @@ public class Usuario {
 	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
 	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties(value = "usuario", allowSetters = true)
-	private List<Produto> produto;
+	//@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	//@JsonIgnoreProperties(value = "usuario", allowSetters = true)
+	//private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -83,13 +81,13 @@ public class Usuario {
 		this.foto = foto;
 	}
 
-	public List<Produto> getProduto() {
-		return produto;
-	}
+	//public List<Produto> getProduto() {
+	//	return produto;
+	//}
 
-	public void setProduto(List<Produto> produto) {
-		this.produto = produto;
-	}
+	//public void setProduto(List<Produto> produto) {
+	//	this.produto = produto;
+	//}
 	
 	
 
